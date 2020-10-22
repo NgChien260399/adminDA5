5e1b5feb209cf894c9fc386da956610bc6c43355
Nguyễn Chiến <66434938+NgChien260399@users.noreply.github.com>
2020-10-22 22:00:24 +0700

    update

---
+++ src/app/models/product.ts
@@ -0,0 +1,9 @@
+export class User {
+    product_name: string;
+    product_desc: string;
+    product_content: string;
+    product_price: number;
+    product_status:number;
+    brand_id:string;
+    category_id:string;
+}
